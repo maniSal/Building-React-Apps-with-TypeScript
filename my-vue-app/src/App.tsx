9c26ff9b8dfd2b555f8b76469c2560e1e5454773
maniSal <selmiamani05@gmail.com>
2025-04-23 19:47:38 +0100

    done

---
+++ my-vue-app/src/App.tsx
@@ -0,0 +1,17 @@
+import Counter from "./componenets/Counter"
+import Greeting from "./componenets/Greeting"
+
+
+function App() {
+  
+
+  return (
+    <>
+     <h1> Checkpoint Building React Apps with TypeScript</h1>
+     <Greeting name="Amani"></Greeting>
+     <Counter></Counter>
+    </>
+  )
+}
+
+export default App
